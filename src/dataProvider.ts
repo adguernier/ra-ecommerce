dc785b879c77cfab4640a1188a3a3b15fbb05c94
adguernier <adrien@marmelab.com>
2023-06-12 16:32:10 +0200

    Add data-generator-retail and use it with ra-data-fakerest as a data provider

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -1,4 +1,5 @@
-import fakeRestDataProvider from 'ra-data-fakerest';
-import data from './data.json';
+import generateData from "data-generator-retail";
+import fakeRestDataProvider from "ra-data-fakerest";
 
+const data = generateData();
 export const dataProvider = fakeRestDataProvider(data, true);
