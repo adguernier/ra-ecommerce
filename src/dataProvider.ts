485fa709febf9d777355981de1e2d4ca411c7ad5
adguernier <adrien@marmelab.com>
2023-06-12 17:09:32 +0200

    fakeRestDataProvider log only in dev env

--- src/dataProvider.ts
+++ src/dataProvider.ts
@@ -2,4 +2,7 @@ import generateData from "data-generator-retail";
 import fakeRestDataProvider from "ra-data-fakerest";
 
 const data = generateData();
-export const dataProvider = fakeRestDataProvider(data, true);
+export const dataProvider = fakeRestDataProvider(
+  data,
+  process.env.NODE_ENV === "development"
+);
