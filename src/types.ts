80774c16eeefed9903902b2ef66ed88745febb63
adguernier <adrien@marmelab.com>
2023-06-14 15:44:33 +0200

    add some uselful interface for resources

---
+++ src/types.ts
@@ -0,0 +1,80 @@
+export interface Order {
+  id: number;
+  reference: string;
+  string: string;
+  customer_id: number;
+  basket: Basket[];
+  total_ex_taxes: number;
+  delivery_fees: number;
+  tax_rate: number;
+  taxes: number;
+  total: number;
+  status: "ordered" | "delivered" | "canceled";
+  returned: boolean;
+}
+
+export interface Basket {
+  product_id: number;
+  quantity: number;
+}
+
+export interface Product {
+  id: number;
+  category_id: number;
+  reference: string;
+  width: number;
+  height: number;
+  price: number;
+  thumbnail: string;
+  image: string;
+  description: string;
+  stock: number;
+}
+
+export interface Customer {
+  id: number;
+  first_name: string;
+  last_name: string;
+  email: string;
+  address: string;
+  zipcode: string;
+  city: string;
+  avatar: string;
+  birthday: string;
+  first_seen: string;
+  last_seen: string;
+  has_ordered: boolean;
+  latest_purchase: string;
+  has_newsletter: boolean;
+  groups: [];
+  nb_commands: number;
+  total_spent: number;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface Invoice {
+  id: number;
+  string: string;
+  command_id: number;
+  customer_id: number;
+  total_ex_taxes: number;
+  delivery_fees: number;
+  tax_rate: number;
+  taxes: number;
+  total: number;
+}
+
+export interface Review {
+  id: number;
+  string: string;
+  status: "pending" | "accepted" | "rejected";
+  command_id: number;
+  product_id: number;
+  customer_id: number;
+  rating: number;
+  comment: string;
+}
