de95c8b63cc59f5874c797710d16dd9b5ca195c1
adguernier <adrien@marmelab.com>
2023-06-14 18:05:25 +0200

    add tabs from Mui

--- src/ui/order/OrderList.tsx
+++ src/ui/order/OrderList.tsx
@@ -1,3 +1,4 @@
+import { SyntheticEvent, useState } from "react";
 import {
   ArrayField,
   BooleanField,
@@ -10,26 +11,92 @@ import {
   SingleFieldList,
   TextField,
 } from "react-admin";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
-export const OrderList = () => (
-  <List>
-    <Datagrid rowClick="edit">
-      <TextField source="id" />
-      <TextField source="reference" />
-      <DateField source="date" />
-      <ReferenceField source="customer_id" reference="customers" />
-      <ArrayField source="basket">
-        <SingleFieldList>
-          <ChipField source="product_id" />
-        </SingleFieldList>
-      </ArrayField>
-      <NumberField source="total_ex_taxes" />
-      <NumberField source="delivery_fees" />
-      <NumberField source="tax_rate" />
-      <NumberField source="taxes" />
-      <NumberField source="total" />
-      <TextField source="status" />
-      <BooleanField source="returned" />
-    </Datagrid>
-  </List>
-);
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+const a11yProps = (index: number) => {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+};
+
+const TabPanel = (props: TabPanelProps) => {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export const OrderList = () => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event: SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Item One" {...a11yProps(0)} />
+          <Tab label="Item Two" {...a11yProps(1)} />
+          <Tab label="Item Three" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        <List>
+          <Datagrid rowClick="edit">
+            <TextField source="id" />
+            <TextField source="reference" />
+            <DateField source="date" />
+            <ReferenceField source="customer_id" reference="customers" />
+            <ArrayField source="basket">
+              <SingleFieldList>
+                <ChipField source="product_id" />
+              </SingleFieldList>
+            </ArrayField>
+            <NumberField source="total_ex_taxes" />
+            <NumberField source="delivery_fees" />
+            <NumberField source="tax_rate" />
+            <NumberField source="taxes" />
+            <NumberField source="total" />
+            <TextField source="status" />
+            <BooleanField source="returned" />
+          </Datagrid>
+        </List>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </Box>
+  );
+};
