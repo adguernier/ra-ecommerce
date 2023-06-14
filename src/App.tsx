bc4485c5b02035d6dde0a33c16e1dd25634f6e47
adguernier <adrien@marmelab.com>
2023-06-14 12:04:06 +0200

    Import missing ListGuesser

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import { Admin, Resource, EditGuesser, ShowGuesser } from "react-admin";
+import {
+  Admin,
+  Resource,
+  EditGuesser,
+  ShowGuesser,
+  ListGuesser,
+} from "react-admin";
 import { dataProvider } from "./dataProvider";
 import { authProvider } from "./authProvider";
 import { AppLayout, InvoiceList } from "./ui";
