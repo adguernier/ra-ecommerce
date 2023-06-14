d97feea8fe91ebcda8afec87fb07c8247ce6ce24
adguernier <adrien@marmelab.com>
2023-06-14 10:05:07 +0200

    add a logo in AppBar

--- src/ui/AppBar.tsx
+++ src/ui/AppBar.tsx
@@ -1,10 +1,11 @@
 import { TitlePortal, AppBar as RaAppBar } from "react-admin";
 import { Box } from "@mui/material";
+import Logo from "./Logo";
 export const AppBar = () => {
   return (
     <RaAppBar color="transparent">
       <TitlePortal />
-      <h4>RA E-commerce</h4>
+      <Logo />
       <Box flex="1" />
     </RaAppBar>
   );
