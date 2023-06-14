93646c51f2ed8cdf96304bd84cac38fdc696955b
adguernier <adrien@marmelab.com>
2023-06-14 15:20:31 +0200

    add margin in basket

--- src/ui/invoice/InvoiceShow.tsx
+++ src/ui/invoice/InvoiceShow.tsx
@@ -8,6 +8,7 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
+import Divider from "@mui/material/Divider";
 import { Basket } from "../order";
 
 export const InvoiceShow = () => {
@@ -21,16 +22,23 @@ export const InvoiceShow = () => {
             spacing={2}
             justifyContent="space-between"
             alignItems="center"
+            sx={{
+              marginBottom: 2,
+            }}
           >
             <Typography variant="h6">Posters Galore</Typography>
             <Typography variant="h6">Invoice {record.id}</Typography>
           </Stack>
-
+          <Divider />
           <Stack
             direction="row"
             spacing={2}
-            justifyContent="space-around"
+            justifyContent="space-between"
             alignItems="center"
+            sx={{
+              marginBottom: 2,
+              marginTop: 2,
+            }}
           >
             <Stack direction="column" spacing={2}>
               <Typography variant="h6">Date</Typography>
