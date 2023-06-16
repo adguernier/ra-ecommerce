e33a35639fc7673f5a1b333040e975336896c541
adguernier <adrien@marmelab.com>
2023-06-16 13:45:25 +0200

    add EditGuesser and ShowGuesser for customer

--- src/ui/customer/index.ts
+++ src/ui/customer/index.ts
@@ -1,7 +1,10 @@
+import { EditGuesser, ShowGuesser } from "react-admin";
 import { CustomerList } from "./CustomerList";
 import { RecordRepresentation } from "./RecordRepresentation";
 
 export const customer: Partial<ResourceProps> = {
   list: CustomerList,
+  edit: EditGuesser,
+  show: ShowGuesser,
   recordRepresentation: RecordRepresentation,
 };
